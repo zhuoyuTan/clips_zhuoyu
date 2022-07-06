b5e75f87b56b3646c7eace641d2d3a967e80d846
zhuoyu tan <zhuoyut96@gmail.com>
2022-07-06 00:02:50 -0700

    edited production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDfXAxcx92BKb3sa9AQGRZvH_y28-HwqXo",
+    authDomain: "clips-dbfe2.firebaseapp.com",
+    projectId: "clips-dbfe2",
+    storageBucket: "clips-dbfe2.appspot.com",
+    appId: "1:83689547713:web:084428a9ac1ffaa8a57140"
+  }
 };

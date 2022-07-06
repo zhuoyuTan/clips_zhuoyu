aec20ac1111dad814509f3ce6abcd323bc41a6e9
zhuoyu tan <zhuoyut96@gmail.com>
2022-07-05 22:49:11 -0700

    finished section16

---
+++ src/app/video/manage/manage.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clip.model';
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.css']
+})
+export class ManageComponent implements OnInit {
+  videoOrder = '1'
+  clips: IClip[] = []
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute,
+    private clipService:ClipService) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+    })
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = [] 
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
+    })
+  }
+
+  sort(event : Event){
+    const { value } = (event.target as HTMLSelectElement)
+    this.router.navigate([],{ 
+      relativeTo: this.route,
+      queryParams:{
+        sort: value
+      }
+    })
+  }
+}
